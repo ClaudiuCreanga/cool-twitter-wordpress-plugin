178aa2c865924f25e3112885f411d1748ee60ef1
Claudiu Creanga <danutempyrium@yahoo.com>
2016-04-14 23:59:19 +0100

    make the options page

--- wp-content/plugins/cool-twitter/main.php
+++ wp-content/plugins/cool-twitter/main.php
@@ -36,17 +36,22 @@ function twitter_settings_page()
     <?php do_settings_sections( 'my-plugin-settings-group' ); ?>
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Accountant Name</th>
+        <th scope="row">Oauth access token</th>
         <td><input type="text" name="accountant_name" value="<?php echo esc_attr( get_option('accountant_name') ); ?>" /></td>
         </tr>
          
         <tr valign="top">
-        <th scope="row">Accountant Phone Number</th>
+        <th scope="row">Oauth access token secret</th>
         <td><input type="text" name="accountant_phone" value="<?php echo esc_attr( get_option('accountant_phone') ); ?>" /></td>
         </tr>
         
         <tr valign="top">
-        <th scope="row">Accountant Email</th>
+        <th scope="row">Consumer key</th>
+        <td><input type="text" name="accountant_email" value="<?php echo esc_attr( get_option('accountant_email') ); ?>" /></td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Consumer secret</th>
         <td><input type="text" name="accountant_email" value="<?php echo esc_attr( get_option('accountant_email') ); ?>" /></td>
         </tr>
     </table>
